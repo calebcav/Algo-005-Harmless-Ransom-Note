40ab4919e5db06a55f1dd8fec499d9ef21b67e40
Caleb <cavinesscaleb@gmail.com>
2019-08-06 09:18:08 -0400

    Done

--- src/main/java/hbcu/stay/ready/algor/Solution.java
+++ src/main/java/hbcu/stay/ready/algor/Solution.java
@@ -1,8 +1,45 @@
 package hbcu.stay.ready.algor;
+import java.util.*;
 
 public class Solution {
 
     public Boolean harmlessRansomNote(String message , String magazineText){
-        return null;
+
+        HashMap<String, Integer> messageDictionary = createDictionary(message);
+        HashMap<String, Integer> magazineTextDictionary = createDictionary(magazineText);
+        for (Map.Entry<String, Integer> entry: messageDictionary.entrySet()){
+            String word = entry.getKey();
+            if(magazineTextDictionary.containsKey(word)){
+                String wordTwo = word;
+                if (messageDictionary.get(word) > magazineTextDictionary.get(wordTwo)){
+                    return false;
+                }
+            }
+        }
+
+
+
+        return true;
     }
+
+    private HashMap<String, Integer> createDictionary(String input){
+
+        HashMap<String, Integer> dictionary = new HashMap<>();
+
+        String[] list = input.split("\\W+");
+
+        for (String word: list){
+            if (!dictionary.containsKey(word)){
+                dictionary.put(word, 1);
+            }
+            else{
+                dictionary.put(word, dictionary.get(word) + 1);
+            }
+        }
+        return dictionary;
+    }
+
+
+
+
 }
